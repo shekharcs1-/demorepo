a56864d76260b233baf1a755c63f5d3eadf1f023
shekharcs1- <shekharcs10@gmail.com>
2017-06-29 16:45:42 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package First;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Test1 t1 = new Test1();
+		t1.Launch();
+		t1.regression();
+		t1.veterinrian();
+		//t1.close();
+
+	}
+
+}
